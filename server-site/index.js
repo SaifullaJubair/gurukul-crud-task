cd267110692acd23ee3fb973b2379631ec7f2277
SaifullaJubair <saifullajubair6@gmail.com>
2024-01-05 22:48:08 +0600

    Add gurukul-client-site and server-site

---
+++ server-site/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.y5svw3w.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+  } finally {
+  }
+}
+run().catch((error) => {
+  console.error(error);
+  res.status(500).json({ error: "Internal server error" });
+});
+
+app.listen(port, () => {
+  console.log(`Gurukul server is running on ${port}`);
+});
